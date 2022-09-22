adbe8653572767607c6a38cad2ea3e9269280a1b
Alec Stein <alec@dolthub.com>
2022-09-22 13:29:16 -0400

    shrank parsre

--- transparency-in-coverage/parsers/flatten_in_network_json.py
+++ transparency-in-coverage/parsers/flatten_in_network_json.py
@@ -4,7 +4,7 @@ import uuid
 import glob
 import os
 
-in_network_file = "PUT_THE_IN_NETWORK_FILE_HERE.json"
+in_network_file = glob.glob("*.json")[0]
 
 def pop(string):
     """
@@ -31,13 +31,15 @@ def walk(prefix, parser, output_dir, **uuids):
     highest level in the JSON) doesn't play nicely
     with writing to file, so I've changed it to "root"
     when needed.
+    
+    The initial event is always 'start_map'
     """
     
     if prefix == '':
         prefix = 'root'
     
     data = {}
-    
+            
     # Add a new UUID field
     uuids[f'{pop(prefix)}_uuid'] = uuid.uuid4()
 
@@ -46,54 +48,56 @@ def walk(prefix, parser, output_dir, **uuids):
     for key, value in uuids.items():
         data[key] = value
 
-    while True:
-        
-        new_row = next(parser)
-        new_prefix, new_event, new_value = new_row
+    new_prefix, new_event, new_value = next(parser)
+    
+    while new_event != 'end_map':
         
         if new_prefix == '':
             new_prefix = 'root'
+            
+            
+        if new_event in ['string', 'number']:
+            data[pop(new_prefix)] = new_value
+            new_prefix, new_event, new_value = next(parser)
+            continue
 
         if new_event == 'start_map':
             # Recurse to the next level down,
             # passing in the uuids from the parents
             walk(new_prefix, parser, output_dir, **uuids)
             
-        if new_event in ['string', 'number']:
-            data[pop(new_prefix)] = new_value
-            
-        if (new_event == 'end_map') and (new_prefix == prefix):
-            # Once we've reached "end map" and the prefix
-            # matches, we've captured everything at this level
-            # in the JSON. Write it to file.
-                
-            output_filename = f"{output_dir}/{prefix}.csv"
-            
-            mode = "w+"
+        new_prefix, new_event, new_value = next(parser)
+                        
+    # Once we've reached "end map" and the prefix
+    # matches, we've captured everything at this level
+    # in the JSON. Write it to file.
 
-            if os.path.exists(output_filename):
-                mode = "a"
-                
-            with open(output_filename, mode) as f:
-                fieldnames = [pop(k) for k in data.keys()]
-                writer = csv.DictWriter(f, fieldnames = fieldnames)
-                
-                if mode == "w+":
-                    writer.writeheader()
-                    
-                writer.writerow(data)
-                
-            break
+    output_filename = f"{output_dir}/{prefix}.csv"
+
+    mode = "w+"
+
+    if os.path.exists(output_filename):
+        mode = "a"
+
+    with open(output_filename, mode) as f:
+        fieldnames = [pop(k) for k in data.keys()]
+        writer = csv.DictWriter(f, fieldnames = fieldnames)
+
+        if mode == "w+":
+            writer.writeheader()
+
+        writer.writerow(data)
         
 
 def tableize_file(filename, output_dir = './flatten'):
     
     if not os.path.exists(output_dir):
+        print('making dir')
         os.mkdir(output_dir)
         
     else:
-        for file in glob.glob(f'output_dir/*'):
-            os.rmdir(file)
+        for file in glob.glob(f'{output_dir}/*'):
+            os.remove(file)
 
     with open(in_network_file, "r") as f:
     
