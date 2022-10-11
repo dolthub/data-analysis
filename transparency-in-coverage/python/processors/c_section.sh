daa2b509b89102716686017f285038578565e066
Alec Stein <alec@dolthub.com>
2022-10-11 17:45:27 -0400

    screen file

--- transparency-in-coverage/python/processors/c_section.sh
+++ transparency-in-coverage/python/processors/c_section.sh
@@ -6,3 +6,5 @@ screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022
 screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_UnitedHealthcare-of-Louisiana--Inc-_Insurer_HML-75_ED_in-network-rates.json.gz
 screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022-10-01/2022-10-01_UnitedHealthcare-of-Utah--Inc-_Insurer_HML-15_S9_in-network-rates.json.gz
 screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022-10-01/2022-10-01_UnitedHealthcare-of-Georgia--Inc-_Insurer_HML-15_S9_in-network-rates.json.gz
+screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_UnitedHealthcare-of-Arkansas--Inc-_Insurer_HML-75_ED_in-network-rates.json.gz
+screen -dm python3 example3.py https://uhc-tic-mrf.azureedge.net/public-mrf/2022-09-01/2022-09-01_UnitedHealthcare-of-Arizona--Inc-_Insurer_HML-75_ED_in-network-rates.json.gz
