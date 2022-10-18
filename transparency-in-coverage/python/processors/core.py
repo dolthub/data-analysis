64b2ca45ba7428a07c81a407c1c4bd86a9fd32b8
Alec Stein <alec@dolthub.com>
2022-10-18 00:07:38 -0400

    dramatically simplified parser code

--- transparency-in-coverage/python/processors/core.py
+++ transparency-in-coverage/python/processors/core.py
@@ -21,9 +21,9 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_mrfs_from_index(index_file_url):
-	'''
+    """
     Gets in-network files from index.json files
-	'''    
+    """
     s = time.time()
     in_network_file_urls = []
 
@@ -99,7 +99,9 @@ def stream_json_to_csv(input_url, output_dir, code_list=None, npi_list=None):
         for prefix, event, value in parser:
             if (prefix, event) == ("in_network.item", "start_map"):
                 row = prefix, event, value
-                innetwork, row = build_innetwork(row, parser, code_list, provref_idx)
+                innetwork, row = build_innetwork(
+                    row, parser, code_list, npi_list, provref_idx
+                )
 
                 if innetwork:
                     innetwork_rows = innetwork_to_rows(innetwork, root_hash_id)
