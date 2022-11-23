847f31f527d19e45ed888d2fd3c31ddb91419275
rl1987 <rl1987@users.noreply.github.com>
2022-11-23 14:11:09 +0800

    Create a simple TF config for server that will run dolt with Jupyter

---
+++ nimby-vs-yimby/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = file("do_token.txt")
+}
+
+data "digitalocean_ssh_keys" "keys" {
+  sort {
+    key       = "name"
+    direction = "asc"
+  }
+}
+
+resource "digitalocean_droplet" "server" {
+  image     = "debian-11-x64"
+  name      = "dolt-jupyter-housingprices"
+  region    = "sfo3"
+  size      = "s-8vcpu-16gb"
+  ssh_keys  = [ for key in data.digitalocean_ssh_keys.keys.ssh_keys: key.fingerprint ]
+  #user_data = file("provision.sh")
+}
+
+output "server_ip" {
+  value = resource.digitalocean_droplet.server.ipv4_address
+}
+
