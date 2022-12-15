bc65bce79173f1b09ea6c6c67c9a1d9765691fc0
Alec Stein <alec@dolthub.com>
2022-12-15 14:26:32 -0500

    added cli example

--- transparency-in-coverage/python/processors/example_cli.py
+++ transparency-in-coverage/python/processors/example_cli.py
@@ -2,23 +2,27 @@ from mrfutils import data_import, flatten_mrf, InvalidMRF
 import argparse
 
 import logging
-logging.basicConfig()  # Add logging level here if you plan on using logging.info() instead of my_logger as below.
+logging.basicConfig()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--url')
 parser.add_argument('-o', '--out')
+parser.add_argument('-c', '--codes')
+parser.add_argument('-n', '--npis')
 
 args = parser.parse_args()
 
-code_set = data_import('test/codes.csv')
-npi_set = {int(x[0]) for x in data_import('test/npis.csv')}
+url = args.url
+out_dir = args.out
+code_set = data_import(args.codes)
+npi_set = {int(x[0]) for x in data_import(args.npis)}
 
 try:
     flatten_mrf(
-        loc=args.url,
-        out_dir=args.out,
-        code_set=code_set,
-        npi_set=npi_set
+        loc = url,
+        out_dir = out_dir,
+        code_set = code_set,
+        npi_set = npi_set
     )
 except InvalidMRF as e:
     log.critical(e)
