5d2910de5575fa6ca21d23957f43545ec0ae7f4d
Alec Stein <alec@dolthub.com>
2022-12-19 14:19:19 -0500

    simplified example2.py

--- transparency-in-coverage/python/processors/example2.py
+++ transparency-in-coverage/python/processors/example2.py
@@ -1,36 +1,26 @@
-import requests
-import time
 import ijson
-from urllib.parse import urlparse
-from helpers import MRFOpen
+from mrfutils import MRFOpen
 
-def mrfs_from_idx(idx_url):
-    '''
+def mrfs_from_idx(index_loc):
+    """
     Gets in-network files from index.json files
-    '''
-    s = time.time()
+    :param idx_url:
+    :return: list of in-network file URLS
+    """
     in_network_file_urls = []
-
-    with MRFOpen(idx_url) as f:
-
+    with MRFOpen(index_loc) as f:
         parser = ijson.parse(f, use_float=True)
-
         for prefix, event, value in parser:
-
             if (
                 prefix.endswith('location')
                 and event == 'string'
+                and 'in-network' in value
             ):
-                print(f'Found in-network file: {value}')
                 in_network_file_urls.append(value)
 
-    td = time.time() - s
-
     print(f'Found: {len(in_network_file_urls)} in-network files.')
-    print(f'Time taken: {round(td/60, 3)} min.')
-
+    print(in_network_file_urls)
     return in_network_file_urls
 
-idx_url = 'https://www.allegiancecosttransparency.com/2022-07-01_LOGAN_HEALTH_index.json'
-
-mrfs_from_idx(idx_url)
+index_loc = 'https://www.allegiancecosttransparency.com/2022-07-01_LOGAN_HEALTH_index.json'
+mrfs_from_idx(index_loc)
