4746bccd318b07f5327e5900d3c29c9217610533
Alec Stein <alecstein@users.noreply.github.com>
2022-12-20 12:37:48 -0500

    Update import.sh

--- transparency-in-coverage/python/processors/import.sh
+++ transparency-in-coverage/python/processors/import.sh
@@ -1,3 +1,7 @@
+# Convenience script for importing the table data in the right order
+# Usage: bash import.sh <out_dir> 
+# <out_dir> is where your root.csv (etc.) files are saved
+
 for table in root codes provider_groups negotiated_rates provider_groups_negotiated_rates_link;
 do
   echo dolt table import -u $table $1/$table.csv
