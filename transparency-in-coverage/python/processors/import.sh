2513e0b196a8042194204a15966f222472747ae9
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-12-21 17:11:18 -0500

    Incorrect table names fixed

--- transparency-in-coverage/python/processors/import.sh
+++ transparency-in-coverage/python/processors/import.sh
@@ -2,7 +2,7 @@
 # Usage: bash import.sh <out_dir> 
 # <out_dir> is where your root.csv (etc.) files are saved
 
-for table in root codes provider_groups negotiated_rates provider_groups_negotiated_rates_link;
+for table in root codes provider_groups negotiated_prices provider_groups_negotiated_prices_link;
 do
   echo dolt table import -u $table $1/$table.csv
 done
