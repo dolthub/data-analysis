b746d0ea4d50541d727007bde953559cf8a4f946
rl1987 <rl1987@users.noreply.github.com>
2022-12-28 11:09:51 +0800

    Remove commented-out code in example3.py

--- transparency-in-coverage/python/processors/example3.py
+++ transparency-in-coverage/python/processors/example3.py
@@ -31,14 +31,6 @@ def save_urls(input_list, save_file='in_network_urls_full.txt'):
     log.info(f"in-network file successfully saved to: {save_file}")
     return
 
-
-# def yield_records(jsonobj, query=None):
-#     if not query:
-#         query = "reporting_structure.item.in_network_files"
-#     for item in ijson.items(jsonobj, query, use_float=True):
-#         yield item
-
-
 def mrfs_from_idx(index_loc):
     """
     A generator that yields in_network URL's for use in your code
@@ -59,7 +51,6 @@ def main():
     parser.add_argument('-u', '--url', help='URL pointing to a TOC index remote json file')
     parser.add_argument('-f', '--input-file', dest='input_file', 
                        help='File of Index URLs to process in bulk')
-    # parser.add_argument('-o', '--out', dest='out_dir', default='out_dir')
     args = parser.parse_args()
 
     targets_list = []
@@ -72,9 +63,6 @@ def main():
         # url = args.url
         targets_list.append(args.url)
     else:
-        # print("[!] You must specify either a url or an input file")
-        # parser.print_help()
-        # sys.exit(1)
         log.info("Did not pass URL or input file, processing example data")
         idx_url = 'https://www.allegiancecosttransparency.com/2022-07-01_LOGAN_HEALTH_index.json'
         # anthem index url: 
@@ -84,18 +72,11 @@ def main():
     rates_urls = []
     for url in targets_list:
         for result in mrfs_from_idx(url):
-            #rates_urls.append(result)
             fobj.write(f"{result.strip()}\n")
     fobj.close()
-    # Once finished, we have a big list of in-network rates URLs
-    #print(f"[*] Finished fetching, collected {len(rates_urls)} in-network URLs in total")
-    #log.info(f"Finished fetching, collected {len(rates_urls)} in-network URLs in total")
     log.info(f"Finished fetching in-network URLs from target file")
-    #save_urls(rates_urls)
-    # save_urls(rates_urls, save_file="urls_file.txt")
     return
 
 
 if __name__ == '__main__':
-    # print("[*] Running example routine to extract in-network files from a test index/TOC URL")
     main()
