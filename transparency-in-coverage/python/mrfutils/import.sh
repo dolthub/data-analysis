a99387b8398bfaa89f19cde3bc4afa8b91b60985
Alec Stein <alec@dolthub.com>
2023-02-15 16:25:37 -0500

    update import.sh

--- transparency-in-coverage/python/mrfutils/import.sh
+++ transparency-in-coverage/python/mrfutils/import.sh
@@ -2,7 +2,7 @@
 # Usage: bash import.sh <out_dir> 
 # <out_dir> is where your root.csv (etc.) files are saved
 
-for table in file insurer code price_metadata rate tin file_rate tin_rate npi_tin;
+for table in file insurer code price_metadata rate tin file_rate tin_rate_file npi_tin;
 do
   echo WRITING TABLE $table
   dolt table import -u $table $1/$table.csv
